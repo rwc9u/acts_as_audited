098867d0109e67c3609f916c4af1ee16e3a65fb2
Pat George <pat.george@gmail.com>
2010-12-28 12:30:54 -0500

    Remove debugging.

--- generators/audit_model/audit_model_generator.rb
+++ generators/audit_model/audit_model_generator.rb
@@ -6,7 +6,6 @@ class AuditModelGenerator < Rails::Generator::NamedBase
   end
 
   def manifest
-    # puts "*** [AuditModelGenerator.manifest] - File.join(File.dirname(__FILE__), '..', '..', 'lib', 'acts_as_audited') = #{File.join(File.dirname(__FILE__), '..', '..', 'lib', 'acts_as_audited')}"
     record do |m|
       m.directory(File.join('app', 'models'))
       m.template('model.rb', "app/models/audit.rb", :assigns => { :human_model => @human_model })
